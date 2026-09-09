7eab1c5651c3366c846fdbddb931ae18da4e9772
wingamingsrl <wingamingsrl@gmail.com>
2026-09-09 14:18:06 +0200

    Rename function carica_da_base_locale to carica_database_locale

--- test_app.py
+++ test_app.py
@@ -138,8 +138,7 @@ def carica_database_locale():
            push_excel_su_github(df_s)   
     return df_l, df_t, df_s
     
-df_locali, df_tecnici, df_storico_file = carica_da_base_locale()
-
+df_locali, df_tecnici, df_storico_file = carica_database_locale()
 
 if "storico_cloud" not in st.session_state:
     st.session_state.storico_cloud = df_storico_file.to_dict('records')
