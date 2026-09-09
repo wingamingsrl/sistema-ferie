4a08e3e62dd7a2d693d95b533d6b0913d25d3e25
wingamingsrl <wingamingsrl@gmail.com>
2026-09-09 14:13:27 +0200

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -135,10 +135,8 @@ def carica_database_locale():
     df_s = df_s.reindex(columns=COLONNE_REALI_UFFICIO).fillna("")
     
     if file_modificato_pulizia and not st.session_state.get("congelamento_sincro_attivo", False):
-       try:
-        push_excel_su_github(df_s)
-    except Exception as e_push_init:
-        st.warning("⚠️ Sincronizzazione iniziale in background in coda...")
+           push_excel_su_github(df_s)
+
         
     return df_l, df_t, df_s
 
