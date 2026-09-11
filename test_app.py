00aedf5700543cf2d385213330209938876ace1f
wingamingsrl <wingamingsrl@gmail.com>
2026-09-11 10:42:46 +0200

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -529,14 +529,16 @@ if submit_button:
                 
                 st.session_state.storico_cloud.append(nuova)
                 df_salva = pd.DataFrame(st.session_state.storico_cloud)
-                df_salva.to_excel(FILE_STORICO_PERMANENTE, index=False)
+                
+                # 🛡️ FIX INSERIMENTO: Allinea rigidamente il nome del file sul disco per forzare l'allineamento cloud
+                df_salva.to_excel("storico_ferie.xlsx", index=False)
                 push_excel_su_github(df_salva)
-
                 
                 st.success("✅ OPERAZIONE COMPLETATA!\n\nPratica registrata correttamente a sistema e notifica e-mail inviata.")
                 st.session_state.form_id += 1
                 time.sleep(4.0)
                 st.rerun()
+
             else:
                 st.error(f"❌ Errore Google SMTP: {risposta_server}. Spedizione e-mail fallita.")
 
